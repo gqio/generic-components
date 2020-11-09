8420e19d2286d014e8130c13841f3048d10e24c8
Gregory Quinio <quinio.gregory@gmail.com>
2020-11-09 14:49:16 +0000

    deploy Mon Nov 09 2020 14:49:16 GMT+0000 (Coordinated Universal Time)

---
+++ 0.1.1/generic-accordion/index.js
@@ -0,0 +1,68 @@
+import { S as SelectedMixin, B as BatchingElement } from '../SelectedMixin-ae217ba9.js';
+import '../keycodes-7142cdc2.js';
+
+const template = document.createElement("template");
+template.innerHTML = `
+  <style>
+    ::slotted(button) {
+      display: block;
+      width: 100%;
+    }
+    :host {
+      display: block;
+    }
+  </style>
+  <slot>
+  </slot>
+`;
+class GenericAccordion extends SelectedMixin(BatchingElement) {
+    static get config() {
+        return {
+            selectors: {
+                buttons: {
+                    selector: (el) => el.querySelectorAll("button"),
+                    focusTarget: true,
+                },
+                regions: {
+                    selector: (el) => el.querySelectorAll("generic-accordion > *:not(button)"),
+                },
+            },
+            multiDirectional: false,
+            orientation: "vertical",
+            shouldFocus: true,
+            activateOnKeydown: false,
+            disabled: false,
+        };
+    }
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+    update() {
+        const { buttons, regions } = this.getElements();
+        buttons.forEach((_, i) => {
+            if (i === this.selected) {
+                this.requestUpdate(true);
+                buttons[i].setAttribute("selected", "");
+                buttons[i].setAttribute("aria-expanded", "true");
+                buttons[i].setAttribute("aria-disabled", "true");
+                regions[i].hidden = false;
+                this.value = buttons[i].textContent.trim();
+            }
+            else {
+                buttons[i].setAttribute("aria-expanded", "false");
+                buttons[i].removeAttribute("aria-disabled");
+                buttons[i].removeAttribute("selected");
+                regions[i].hidden = true;
+            }
+            if (!buttons[i].id.startsWith("generic-accordion-")) {
+                buttons[i].id = `generic-accordion-${this.__uuid}-${i}`;
+                regions[i].setAttribute("aria-labelledby", `generic-accordion-${this.__uuid}-${i}`);
+                regions[i].setAttribute("role", "region");
+            }
+        });
+    }
+}
+
+customElements.define("generic-accordion", GenericAccordion);
